3f992df2fa3f7dd4eeacaf5ac05037af7c7e5d35
Nohan550 <nohanmuttakim.bd@gmail.com>
2023-11-05 18:09:41 +0600

    project created

---
+++ src/MyRoutes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>hello world</div>,
+    },
+  ]);
+ 
+
+export default routes;
